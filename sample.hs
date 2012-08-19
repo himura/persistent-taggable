ee6222597f2520742e6fc136b99ee6dc2cb0df09
Takahiro HIMURA <taka@himura.jp>
2012-08-19 23:33:37 +0900

    Added sample.hs

---
+++ sample.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+import Database.Persist
+import Database.Persist.TH
+import Database.Persist.Sqlite
+import Database.Persist.Query.Taggable.Sql
+import qualified Data.Conduit as C
+import qualified Data.Conduit.List as CL
+import qualified Data.Text as T
+import Control.Monad.IO.Class
+import Control.Monad
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persist|
+Language
+    fullname T.Text Eq
+    deriving Show
+Tag
+    name T.Text Update Eq
+    UniqueTagNameKey name
+    deriving Show
+LanguageTag
+    language LanguageId Eq
+    tag TagId Eq
+    UniqueLanguageTag language tag
+    deriving Show
+|]
+
+main :: IO ()
+main = withSqliteConn ":memory:" . runSqlConn $ do
+    runMigration migrateAll
+
+    haskell <- insert $ Language "Haskell"
+    ocaml <- insert $ Language "OCaml"
+    scala <- insert $ Language "Scala"
+    java <- insert $ Language "Java"
+
+    pure <- insert $ Tag "Pure"
+    functional <- insert $ Tag "Functional"
+    jvm <- insert $ Tag "JVM"
+    native <- insert $ Tag "Native"
+
+    forM_ [haskell, ocaml, scala] $ \lang -> do
+        insert $ LanguageTag lang functional
+
+    forM_ [java, scala] $ \lang -> do
+        insert $ LanguageTag lang jvm
+
+    forM_ [ocaml, haskell] $ \lang -> do
+        insert $ LanguageTag lang native
+
+    void . insert $ LanguageTag haskell pure
+
+    let query = (taggable [functional, native]
+                 (LanguageTagTag <-.)
+                 (LanguageTagLanguage ==. undefined))
+                { taggableFilters = [] :: [Filter Language]
+                }
+    C.runResourceT
+        $    selectTaggableSource query
+        C.$$ CL.mapM_ $ \lang -> do
+            liftIO . print . entityVal $ lang
